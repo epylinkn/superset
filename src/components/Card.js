6d89a7ccedf272820c2c0ee6810eedcef5e82aa3
Anthony Bui <anthonybui@gmail.com>
2017-03-02 16:28:04 -0500

    Build deck, sample board, display card attributes

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,38 @@ import Relay from 'react-relay';
 import _ from 'lodash';
 
 class Card extends Component {
+  iconFor(shape, fill) {
+    if (shape == "battery") {
+      return {
+        "empty": "fa-battery-empty",
+        "half": "fa-battery-half",
+        "full": "fa-battery-full"
+      }[fill];
+    } else if (shape == "star") {
+      return {
+        "empty": "fa-star-o",
+        "half": "fa-star-half-o",
+        "full": "fa-star"
+      }[fill];
+    } else {
+      return {
+        "empty": "fa-circle-o",
+        "half": "fa-dot-circle-o",
+        "full": "fa-circle"
+      }[fill];
+    }
+  }
+
+
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
-      <div className="card">
-        {_.times(_.random(2)+1, (i) =>
-          <i className="fa fa-battery-empty fa-5x" aria-hidden="true"></i>
+      <div className={`card card-${this.props.color}`}>
+        {_.times(this.props.number, (i) =>
+          <i className={`fa ${this.iconFor(this.props.shape, this.props.fill)} fa-5x`}></i>
         )}
       </div>
     );
--- src/components/Game.js
+++ src/components/Game.js
@@ -4,14 +4,51 @@ import _ from 'lodash';
 import Card from './Card';
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    var deck = this.createDeck();
+    this.state = {
+      deck: deck,
+      board: _.sample(deck, 12)
+    }
+
+    console.log("BOARD", this.state.board);
+  }
+
+  createDeck() {
+    const colors = ["cyan", "magenta", "yellow"];
+    const numbers = [1, 2, 3];
+    const shapes = ["star", "circle", "battery"];
+    const fills =  ["empty", "half", "full"];
+
+    var cards = [];
+    _.each(colors, (color) => {
+      _.each(numbers, (number) => {
+        _.each(shapes, (shape) => {
+          _.each(fills, (fill) => {
+            cards.push({
+              id: cards.length,
+              color: color,
+              number: number,
+              shape: shape,
+              fill: fill
+            })
+          })
+        })
+      })
+    })
+
+    return _.shuffle(cards);
+  }
 
   render() {
     return (
       <div className="container">
         <div className="row">
-          {_.times(12, (i) =>
+          {this.state.board.map((card) =>
             <div className="col s3">
-              <Card />
+              <Card color={card.color} number={card.number} shape={card.shape} fill={card.fill} />
             </div>
           )}
         </div>
